a2125cb0cb95ad38be0a6d2e44552bfd456ff3ac
lvris <lvris@qq.com>
2024-10-08 23:57:31 +0300

    ➕ UnoCSS

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'virtual:uno.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,8 @@
 <template>
-  Meal Planner
+  <div text-center font-lobster p4>
+    <div logo />
+    <div text-green5 text-lg>
+      Meal Planer
+    </div>
+  </div>
 </template>
