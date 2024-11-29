f78d859f0a495afa49fd51626e5bb8677dd7444b
lvris <lvris@qq.com>
2024-11-29 15:38:08 +0200

    💄 TrySomething in Home

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <HeaderBar title="Dashboard"></HeaderBar>
 
-  <NCarousel>
+  <NCarousel h-80>
     <img class="carousel-img"
       src="https://scatterjar.com/wp-content/uploads/2019/10/www.scatterjar.com-3518-1024x1536.jpg">
     <img class="carousel-img"
@@ -11,6 +11,9 @@
     <img class="carousel-img"
       src="https://scatterjar.com/wp-content/uploads/2016/07/www.scatterjar.com-2306-1024x683.jpg">
   </NCarousel>
+
+  <NResult status="404" title="Try Something">
+  </NResult>
 </template>
 
 <script lang="ts" setup>
